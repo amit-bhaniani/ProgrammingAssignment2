8fec0bed50f57e203f49057d41c58ca11fab260b
Amit Bhaniani <amit_bh@hotmail.com>
2021-04-18 00:44:33 +0100

    second commit.

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,10 @@
 ## this program is to write two functions: makeCacheMatrix and cacheSolve
 
 ## function makeCacheMatrix - set the matrix and get the value of this matrix
+## set the value of the matrix
+## get the value of the matrix
+## set the value of the matrix inverse
+## get the value of the matrix inverse
 
 makeCacheMatrix <- function(x = matrix()) {
         minv <- NULL
@@ -21,12 +25,13 @@ makeCacheMatrix <- function(x = matrix()) {
 ## or print cached inverse of the matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## Return a matrix that is the inverse of 'x' from cache
         minv <- x$getinv()
         if(!is.null(minv)) {
                 message("getting cached data")
                 return(minv)
         }
+        ## Calculate the inverse of the matrix if it does not exist in the cache
         data <- x$get()
         minv <- solve(data, ...)
         x$setinv(minv)
